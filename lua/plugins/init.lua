7a5f44e8e163144bc8112e3f8d24e475e175dfbd
Seong Yong-ju <sei40kr@gmail.com>
2024-08-31 15:17:38 +0900

    feat(bufferline): bind m-h/m-l/m-s-h/m-s-l to cycle/move bufferline

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,1 +1,11 @@
-return {}
+return {
+  {
+    "bufferline.nvim",
+    keys = {
+      { "<M-Left>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev buffer" },
+      { "<M-Right>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next buffer" },
+      { "<M-S-Left>", "<Cmd>BufferLineMovePrev<CR>", desc = "Move buffer prev" },
+      { "<M-S-Right>", "<Cmd>BufferLineMoveNext<CR>", desc = "Move buffer next" },
+    },
+  },
+}
