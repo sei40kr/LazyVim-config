da164137167f74bd0f2b1d82a366eedfbebd26bb
Seong Yong-ju <sei40kr@gmail.com>
2024-10-18 04:27:13 +0900

    feat: add descriptions to keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,9 +6,9 @@ vim.keymap.set("n", "[<Space>", function()
   local lines = vim.fn["repeat"]({ "" }, vim.v.count1)
   local current_line = vim.api.nvim_win_get_cursor(0)[1]
   vim.api.nvim_buf_set_lines(0, current_line - 1, current_line - 1, true, lines)
-end)
+end, { desc = "Add blank lines above" })
 vim.keymap.set("n", "]<Space>", function()
   local lines = vim.fn["repeat"]({ "" }, vim.v.count1)
   local current_line = vim.api.nvim_win_get_cursor(0)[1]
   vim.api.nvim_buf_set_lines(0, current_line, current_line, true, lines)
-end)
+end, { desc = "Add blank lines below" })
