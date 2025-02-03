d7f00d09405cc1ce4d0d2e7c14754ed72ed40d26
Seong Yong-ju <sei40kr@gmail.com>
2025-02-04 00:44:56 +0900

    chore(fzf-lua,neo-tree,nvim-cmp): remove unused plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -73,102 +73,6 @@ return {
     optional = true,
   },
   {
-    "fzf-lua",
-    dependencies = { "nvim-telescope/telescope-symbols.nvim" },
-    keys = function(_, keys)
-      ---@param sources string[]
-      ---@return nil
-      local function insert_symbol(sources)
-        ---@type string[]
-        local files = vim.tbl_filter(function(file)
-          return vim.tbl_contains(sources, vim.fn.fnamemodify(file, ":t:r")) and vim.fn.filereadable(file) == 1
-        end, vim.api.nvim_get_runtime_file("data/telescope-sources/*.json", true))
-
-        require("fzf-lua").fzf_exec(function(fzf_cb)
-          coroutine.wrap(function()
-            local co = coroutine.running()
-            for _, file in ipairs(files) do
-              ---@diagnostic disable: redefined-local
-              vim.uv.fs_open(file, "r", 438, function(err, fd)
-                assert(not err, err)
-                vim.uv.fs_fstat(fd, function(err, stat)
-                  assert(not err, err)
-                  ---@cast stat uv.fs_stat.result
-                  vim.uv.fs_read(fd, stat.size, 0, function(err, data)
-                    assert(not err, err)
-                    ---@type { [1]: string, [2]: string }[]
-                    local entries = vim.json.decode(data)
-                    for _, entry in ipairs(entries) do
-                      fzf_cb(entry[1] .. " " .. entry[2])
-                    end
-                    vim.uv.fs_close(fd, function()
-                      coroutine.resume(co)
-                    end)
-                  end)
-                end)
-              end)
-              ---@diagnostic enable: redefined-local
-              coroutine.yield()
-            end
-            fzf_cb()
-          end)()
-        end, {
-          winopts = {
-            title = " Symbols ",
-            title_pos = "center",
-          },
-          actions = {
-            default = function(selected)
-              vim.cmd([[startinsert]])
-              for _, entry in ipairs(selected) do
-                local symbol = entry:match("^%S+")
-                vim.api.nvim_put({ symbol }, "", true, true)
-              end
-            end,
-          },
-        })
-      end
-
-      keys[#keys + 1] = {
-        "<leader>ie",
-        function()
-          insert_symbol({ "emoji" })
-        end,
-        desc = "Emoji",
-      }
-      keys[#keys + 1] = {
-        "<leader>im",
-        function()
-          insert_symbol({ "math" })
-        end,
-        desc = "Math Symbol",
-      }
-      keys[#keys + 1] = {
-        "<leader>in",
-        function()
-          insert_symbol({ "nerd" })
-        end,
-        desc = "Nerd Font Symbol",
-      }
-      keys[#keys + 1] = {
-        "<leader>ig",
-        function()
-          insert_symbol({ "gitmoji" })
-        end,
-        desc = "Gitmoji",
-        ft = "gitcommit",
-      }
-      keys[#keys + 1] = {
-        "<leader>ij",
-        function()
-          insert_symbol({ "julia" })
-        end,
-        desc = "Julia Unicode Symbol",
-        ft = "julia",
-      }
-    end,
-  },
-  {
     "gitsigns.nvim",
     opts = {
       attach_to_untracked = true,
@@ -199,49 +103,9 @@ return {
     enabled = false,
   },
   {
-    "neo-tree.nvim",
-    dependencies = { "sei40kr/neo-tree-evil-mappings.nvim" },
-    opts = function(_, opts)
-      local evil_mappings = require("neo-tree-evil-mappings")
-      opts.sources = { "filesystem" }
-      opts.use_popups_for_input = false
-      opts.commands = vim.tbl_extend("force", opts.commands or {}, evil_mappings.commands)
-      opts.window = opts.window or {}
-      opts.window.mappings = vim.tbl_extend("force", opts.window.mappings or {}, evil_mappings.mappings)
-      opts.filesystem = opts.filesystem or {}
-      opts.filesystem.filtered_items = {
-        visible = false,
-        hide_dotfiles = false,
-        hide_gitignored = false,
-        hide_by_name = { ".git" },
-      }
-      opts.filesystem.window = opts.filesystem.window or {}
-      opts.filesystem.window.mappings =
-        vim.tbl_extend("force", opts.filesystem.window.mappings or {}, evil_mappings.filesystem_mappings)
-    end,
-    optional = true,
-  },
-  {
     "noice.nvim",
     opts = function(_, opts)
       opts.presets.lsp_doc_border = true
-    end,
-    optional = true,
-  },
-  {
-    "nvim-cmp",
-    opts = function(_, opts)
-      local cmp = require("cmp")
-      return vim.tbl_deep_extend("force", opts, {
-        completion = {
-          completeopt = "menu,menuone,noinsert,noselect",
-        },
-        mapping = {
-          ["<CR>"] = LazyVim.cmp.confirm({ select = false }),
-          ["<C-j>"] = cmp.mapping.select_next_item(),
-          ["<C-k>"] = cmp.mapping.select_prev_item(),
-        },
-      })
     end,
     optional = true,
   },
