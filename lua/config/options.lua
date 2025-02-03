2de45f2ad6b717eddbe2582b14849359c0fdd0df
Seong Yong-ju <sei40kr@gmail.com>
2025-02-04 02:43:38 +0900

    feat(options): configure root_spec and disable swapfile

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,4 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.root_spec = { ".git", "cwd" }
+
 vim.g.lazyvim_python_lsp = "basedpyright"
+
+local opt = vim.opt
+
+-- FIXME: 'swapfile' causes nil reference errors in Neovim 0.11. neovim/neovim#33224
+opt.swapfile = false
